8e3c8df9c06d87fc1612ac80baa63f60e52bc106
André Rocha <rocha.matcomp@gmail.com>
2024-06-19 17:24:11 -0300

    Change root return as HTML

--- notas_musicais_api/app.py
+++ notas_musicais_api/app.py
@@ -4,10 +4,21 @@ para ajudar na formação de escalas, acordes e campos harmônicos.
 """
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_class=HTMLResponse)
 def get_root():
-    return {'message': 'Olá, Notas musicais!'}
+    message = """<html>
+        <head>
+            <title> Olá, Notas musicais!</title>
+        </head>
+        <body>
+            <h1> Olá, Notas musicais! </h1>
+        </body>
+        </html>
+    """
+
+    return message
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,10 +9,20 @@ from fastapi.testclient import TestClient
 from notas_musicais_api.app import app
 
 
-def test_root_must_response_hello_message():
+def test_root_must_response_hello_message_as_html():
     client = TestClient(app)
 
     response = client.get('/')
 
+    message = """<html>
+        <head>
+            <title> Olá, Notas musicais!</title>
+        </head>
+        <body>
+            <h1> Olá, Notas musicais! </h1>
+        </body>
+        </html>
+    """
+
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá, Notas musicais!'}
+    assert response.text == message
